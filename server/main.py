3d0495628f8fc4bb1bd5c16fe19120ed3f1515fd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-04 09:13:59 +0530

    Update

---
+++ server/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+# from routes.ItemRoute import router as ItemRouter
+
+app = FastAPI()
+
+# app.include_router(ItemRouter, prefix='/items')
+
+@app.get('/')
+def read_main():
+    return {"message": "Backend is Working fine...."}
